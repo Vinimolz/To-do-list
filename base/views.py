bb29a45a3b1849bd1ae55aeed7ac078457c5b5d1
Vinimolz <76082483+Vinimolz@users.noreply.github.com>
2023-07-30 10:26:51 -0700

    Added search functionality for to-do list

--- base/views.py
+++ base/views.py
@@ -51,6 +51,12 @@ class TaskList(LoginRequiredMixin, ListView):
         context['count'] = context['tasks'].filter(complete=False).count()
         context['color'] = 'red'
 
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['tasks'] = context['tasks'].filter(title__startswith=search_input)
+
+        context['search_input'] = search_input
+
         return context
     
 class TaskDetail(LoginRequiredMixin, DetailView):
